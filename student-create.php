f4b3f2dfcb361ad4b9316e349d9faf8c08590153
al-jabir <aljabir098@gmail.com>
2023-02-18 01:26:27 +0600

    still running

--- student-create.php
+++ student-create.php
@@ -10,11 +10,11 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Student Create</title>
 </head>
 
 <body>
-    <div class="container">
+    <div class="container mt-5">
         <div class="row">
             <div class="col-md-12">
                 <div class="card">
@@ -24,8 +24,26 @@
                         </h4>
                     </div>
                     <div class="card-body">
-                        <form action="" method="POST">
-                            <label for=""></label>
+                        <form action="data.php" method="POST">
+                            <div class="mb-3">
+                                <label>Student Name</label>
+                                <input type="text" name="name" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Student Email</label>
+                                <input type="email" name="email" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Student phone</label>
+                                <input type="text" name="phone" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <label>Student Course</label>
+                                <input type="text" name="course" class="form-control" />
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" name="save_student" class="btn btn-primary">Save Student</button>
+                            </div>
                         </form>
                     </div>
                 </div>
